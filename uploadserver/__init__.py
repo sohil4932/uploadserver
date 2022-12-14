58674df92356eb476c09dc683448689dd0656441
Sohil Patel <sohilpatel4932@gmail.com>
2022-12-15 02:51:55 +0530

    post api added

--- uploadserver/__init__.py
+++ uploadserver/__init__.py
@@ -197,7 +197,7 @@ def receive_upload(handler):
 class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Access-Control-Allow-Methods', 'POST')
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
         return super(SimpleHTTPRequestHandler, self).end_headers()
     
