6992bdbf469c7afff94670ec407d42a525268e64
Sohil Patel <sohilpatel4932@gmail.com>
2022-12-15 23:01:10 +0530

    added crontab command

--- kiosk.sh
+++ kiosk.sh
@@ -10,3 +10,5 @@ unclutter &
 firefox --window-size=1920,1080 --kiosk --window-position=0,0 http://localhost/camera?booth=booth1 &
 
 cd /home/psm/uploadserver; python3 -m uploadserver --directory=/home/psm/booth/ &
+
+#*/5 * * * * rm /home/psm/booth/*
